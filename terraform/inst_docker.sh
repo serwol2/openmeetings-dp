608423072b5a3db0ed8922b9960d7ea639d89cc4
serwol <adamal_serg@tut.by>
2022-06-11 15:28:56 +0300

    15-5.0.0 min test

--- terraform/inst_docker.sh
+++ terraform/inst_docker.sh
@@ -3,4 +3,5 @@
 sudo apt update && sudo apt install docker.io -y
 
 #docker pull ghcr.io/serwol2/openmeetings-dp:feature-v1.0.0 && 
-docker pull ghcr.io/serwol2/openmeetings-dp/openmeetings-dp:feature-v1.0.0 #&& sudo docker run -i --restart=always --network host ghcr.io/serwol2/openmeetings-dp/openmeetings-dp:feature-v1.0.0
+#docker pull ghcr.io/serwol2/openmeetings-dp/openmeetings-dp:feature-v1.0.0 #&& sudo docker run -i --restart=always --network host ghcr.io/serwol2/openmeetings-dp/openmeetings-dp:feature-v1.0.0
+docker pull ghcr.io/serwol2/openmeetings-dp/openmeetings-dp:feature-v3.0.0-min-old
